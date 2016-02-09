2a7f54b080dda91a9fb928dc5ccfd05f916fa868
ashlen-renner <ashlenrenner@gmail.com>
2016-02-09 04:44:22 -0500

    Halfway through data

--- js/data.js
+++ js/data.js
@@ -130,6 +130,120 @@ bio: "Black Republican was a newspaper that promoted African-American polotics, 
 storyLink: "http://infoweb.newsbank.com/iw-search/we/HistArchive/HistArchive?d_viewref=doc&p_docnum=-1&p_nbid=S4CA45ECMTQ1NTAwMTEyNy45Mzg1MjoxOjE0OjE1Mi4yMy4xMTAuMTU0&f_docref=v2:12EFC46496A73990@EANX-12F12F5B22C07048@2402342-12EFCD20D6214C68@0&p_docref=v2:12EFC46496A73990@EANX-12F12F5B22C07048@2402342-12EFCD20D6214C68@0",
 storyTitle: "Bloody Chapter of Guerrilla Life in Arkansas"
 },
+{
+name: "L'Union",
+startDate: "September 27, 1862",
+endDate: " July 19, 1864",
+numOfIssues: 98,
+location: 'New Orleans, Louisiana',
+editor: 	'Paul Trévigne',
+    images: {
+      editorPic: "unknown",
+      generalPic: "https://roudanez.files.wordpress.com/2015/08/lunion.png?w=219&h=300"
+          },
+imgCaption: "Paul Trévigne was part of the free people of color community in Louisiana that protested racial injustice before the Civil War and helped establish Republican politics in the state after 1865.",
+bio: "L'Union was founded and circulated as a biweekly and tri-weekly. Published primarily in French, the paper ran a few issues in English beginning in 1863. ",
+storyLink: "http://infoweb.newsbank.com/iw-search/we/HistArchive/HistArchive?d_viewref=doc&p_docnum=-1&p_nbid=S4CA45ECMTQ1NTAwMTEyNy45Mzg1MjoxOjE0OjE1Mi4yMy4xMTAuMTU0&f_docref=v2:12B8668E0EB23CD0@EANX-12CC4C75ECF0C2C0@2401411-12CC2CC259330CC0@0&p_docref=v2:12B8668E0EB23CD0@EANX-12CC4C75ECF0C2C0@2401411-12CC2CC259330CC0@0",
+storyTitle: "L'esclavage"
+},
+{
+name: "New Orleans Tribune",
+startDate: "July 21, 1864",
+endDate: " February 28, 1869",
+numOfIssues: 691,
+location: 'New Orleans, Louisiana',
+editor: 	'Charles Louise Roudanez',
+    images: {
+      editorPic: "http://www.blackpast.org/files/blackpast_images/harles_Roudanez__NYPL_Photo_Digital_Library_.jpg",
+      generalPic: "https://roudanez.files.wordpress.com/2015/08/lunion.png?w=219&h=300"
+          },
+imgCaption: "When the Union army took New Orleans in 1862 during the Civil War, Roudanez saw that the time was right to demand equality for free men of color. He started the militant Republican journal, L’Union, published in French and edited by Paul Trevigne. It was the first black newspaper to be published in Louisiana. When L’Union failed in 1864 from lack of support, Roudanez established a new journal called La Tribune de la Nouvelle Orléans (The New Orleans Tribune). ",
+bio: " The New Orleans Tribune was the first African American daily newspaper in the United States. Started in 1864 by Charles Louise Roudanez the Tribune was notable in that it was bilingual.",
+storyLink: "http://infoweb.newsbank.com/iw-search/we/HistArchive/HistArchive?d_viewref=doc&p_docnum=-1&p_nbid=S4CA45ECMTQ1NTAwMTEyNy45Mzg1MjoxOjE0OjE1Mi4yMy4xMTAuMTU0&f_docref=v2:12B3C01E490E6BA0@EANX-12BC6E8AD45FBE78@2402079-12BBC74193F851E8@0&p_docref=v2:12B3C01E490E6BA0@EANX-12BC6E8AD45FBE78@2402079-12BBC74193F851E8@0",
+storyTitle: "Official Report on the Freedmen"
+},
+{
+name: "Elevator",
+startDate: "May 05, 1865",
+endDate: "June 11, 1898",
+numOfIssues: 274,
+location: 'San Francisco, California',
+editor: 	'Philip Alexander Bell',
+    images: {
+      editorPic: "http://www.blackpast.org/files/blackpast_images/bell_philip.jpg",
+      generalPic: "https://roudanez.files.wordpress.com/2015/08/lunion.png?w=219&h=300"
+          },
+imgCaption: "After migrating to San Francisco, California in 1860, Bell maintained his connections with important abolition leaders such as Garrison and Frederick Douglass by reporting on black political and economic opportunities in the West.",
+bio: "The Elevator demanded California legislators approve the proposed Reconstruction-era constitutional amendments acknowledging black citizenship and suffrage rights.",
+storyLink: "http://infoweb.newsbank.com/iw-search/we/HistArchive/HistArchive?d_viewref=doc&p_docnum=-1&p_nbid=S4CA45ECMTQ1NTAwMTEyNy45Mzg1MjoxOjE0OjE1Mi4yMy4xMTAuMTU0&f_docref=v2:12B2891B22FC96B8@EANX-12C8B86AE9FA8948@2402362-12C8B86AF5D13CA8@0&p_docref=v2:12B2891B22FC96B8@EANX-12C8B86AE9FA8948@2402362-12C8B86AF5D13CA8@0",
+storyTitle: "Humiliation of South Carolina"
+},
+{
+name: "Colored American",
+startDate: "December 30, 1865",
+endDate: "January 13, 1866",
+numOfIssues: 3,
+location: 'Augusta, Georgia',
+editor: 	'unknown',
+    images: {
+      editorPic: "unknown",
+      generalPic: ""
+          },
+imgCaption: "unknown",
+bio: "One of the first African-American newspapers to be established in Georgia.",
+storyLink: "http://infoweb.newsbank.com/iw-search/we/HistArchive/HistArchive?d_viewref=doc&p_docnum=-1&p_nbid=S4CA45ECMTQ1NTAwMTEyNy45Mzg1MjoxOjE0OjE1Mi4yMy4xMTAuMTU0&f_docref=v2:12B7C5AD9503CA90@EANX-12CC88154D689248@2402601-12CBE7DE3190F700@0&p_docref=v2:12B7C5AD9503CA90@EANX-12CC88154D689248@2402601-12CBE7DE3190F700@0",
+storyTitle: "Terrible and Swift Justice"
+},
+{
+name: "Loyal Georgian",
+startDate: "January 20, 1866",
+endDate: "February 15, 1868",
+numOfIssues: 16,
+location: 'Augusta, Georgia',
+editor: 	'unknown',
+    images: {
+      editorPic: "unknown",
+      generalPic: "http://hd.housedivided.dickinson.edu/files/images/HD_NwspprH650121p036c.preview.jpg"
+          },
+imgCaption: "unknown",
+bio: "The Loyal Georgian was a weekly newspaper promoting African-American rights.",
+storyLink: "http://infoweb.newsbank.com/iw-search/we/HistArchive/HistArchive?d_viewref=doc&p_docnum=-1&p_nbid=S4CA45ECMTQ1NTAwMTEyNy45Mzg1MjoxOjE0OjE1Mi4yMy4xMTAuMTU0&f_docref=v2:12B7C5E4F02A3B10@EANX-12CC8265AB449578@2402622-12CBE7ADA7BF1068@0&p_docref=v2:12B7C5E4F02A3B10@EANX-12CC8265AB449578@2402622-12CBE7ADA7BF1068@0",
+storyTitle: "General Tillson's Speech"
+},
+{
+name: "Colored Citizen",
+startDate: "May 19, 1866",
+endDate: "May 19, 1866",
+numOfIssues: 1,
+location: 'Cincinnati, Ohio',
+editor: 	'Joseph C. Corbin, John P. Sampson, Charles W. Bell',
+    images: {
+      editorPic: "unknown",
+      generalPic: "http://chroniclingamerica.loc.gov/lccn/sn83045474/1915-12-30/ed-1/seq-2/thumbnail.jpg"
+          },
+imgCaption: "The editors considered the press as the most powerful means of refuting lies and slanders of critics of African Americans.",
+bio: "The newspaper was published by an association of black residents of Cincinnati, Louisville, Zanesville, St. Louis, Chicago, Columbus, Indianapolis and other major cities of the Midwest. It was primarily a journal of general news and literature with an emphasis on news pertinent to African Americans.",
+storyLink: "http://infoweb.newsbank.com/iw-search/we/HistArchive/HistArchive?d_viewref=doc&p_docnum=-1&p_nbid=S4CA45ECMTQ1NTAwMTEyNy45Mzg1MjoxOjE0OjE1Mi4yMy4xMTAuMTU0&f_docref=v2:12B7C0F55673B228@EANX-12C1AED850642AC0@2402741-12C10539E4A3A768@0&p_docref=v2:12B7C0F55673B228@EANX-12C1AED850642AC0@2402741-12C10539E4A3A768@0",
+storyTitle: "The Cholera"
+},
 
+
+
+{
+name: "",
+startDate: "",
+endDate: "",
+numOfIssues: 274,
+location: '',
+editor: 	'',
+    images: {
+      editorPic: "",
+      generalPic: ""
+          },
+imgCaption: "",
+bio: "",
+storyLink: "",
+storyTitle: ""
+},
 
   ];
