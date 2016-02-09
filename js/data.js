970886ec61a5362dd18732f3fc1bbeb0fae9255b
ashlen-renner <ashlenrenner@gmail.com>
2016-02-09 01:56:57 -0500

    initial commit.

---
+++ js/data.js
@@ -0,0 +1,18 @@
+var data = [
+  {
+  name: "Freedom's Journal",
+  startDate: 1827,
+  endDate: 1829,
+  location: 'New York City',
+  editor: 	'John Russwurm and Samuel Cornish',
+      images: {
+        editorPic: "http://www.nathanielturner.com/images/New_Folder/freejour.jpg",
+        generalPic: "http://www.blackpast.org/files/blackpast_images/Freedom_s_Journal.jpg"
+            },
+  imgCaption: 'John Russwurm and Samuel Cornish founded the first African American newspaper in the U.S.',
+  bio: "Freedom's Journal was the first newspaper owned and operated by African Americans published in the U.S.",
+  storyLink: "http://infoweb.newsbank.com/iw-search/we/HistArchive/HistArchive?d_viewref=doc&p_docnum=-1&p_nbid=J49C45SCMTQ1NDYxNjQxNy4zMDQ1NzA6MToxMzoxNTIuMjMuNDkuMTA0&f_docref=v2:132FB88A16969E1C@EANX-132FC89EEDB64928@2388432-132FC0E68E066888@0&p_docref=v2:132FB88A16969E1C@EANX-132FC89EEDB64928@2388432-132FC0E68E066888@0",
+    storyTitle: "This is a Story"
+
+  }
+  ];
