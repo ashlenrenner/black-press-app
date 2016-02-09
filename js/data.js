b3e49c123fd9cff70d30f708dc98fad9283c7689
ashlen-renner <ashlenrenner@gmail.com>
2016-02-09 03:44:29 -0500

    Generating data.

--- js/data.js
+++ js/data.js
@@ -1,18 +1,135 @@
 var data = [
   {
   name: "Freedom's Journal",
-  startDate: 1827,
-  endDate: 1829,
-  location: 'New York City',
+  startDate: "March 16, 1827",
+  endDate: "March 28, 1829",
+  numOfIssues: 103,
+  location: 'New York, New York',
   editor: 	'John Russwurm and Samuel Cornish',
       images: {
         editorPic: "http://www.nathanielturner.com/images/New_Folder/freejour.jpg",
         generalPic: "http://www.blackpast.org/files/blackpast_images/Freedom_s_Journal.jpg"
             },
   imgCaption: 'John Russwurm and Samuel Cornish founded the first African American newspaper in the U.S.',
-  bio: "Freedom's Journal was the first newspaper owned and operated by African Americans published in the U.S.",
+  bio: "Freedom's Journal was the first newspaper owned and operated by African Americans published in the U.S. Established the same year slavery was abolished, The Freedom's Journal served to counter the racist mainstream media.",
   storyLink: "http://infoweb.newsbank.com/iw-search/we/HistArchive/HistArchive?d_viewref=doc&p_docnum=-1&p_nbid=J49C45SCMTQ1NDYxNjQxNy4zMDQ1NzA6MToxMzoxNTIuMjMuNDkuMTA0&f_docref=v2:132FB88A16969E1C@EANX-132FC89EEDB64928@2388432-132FC0E68E066888@0&p_docref=v2:132FB88A16969E1C@EANX-132FC89EEDB64928@2388432-132FC0E68E066888@0",
-    storyTitle: "This is a Story"
+  storyTitle: "Nameplate"
 
-  }
+},
+{
+name: "Rights of All",
+startDate: "May 29, 1829",
+endDate: "October 09, 1829",
+numOfIssues: 5,
+location: 'New York, New York',
+editor: 	'Samuel Cornish',
+    images: {
+      editorPic: "http://www.blackpast.org/files/blackpast_images/Samuel_Cornish_photo.jpg",
+      generalPic: ""
+          },
+imgCaption: "Samuel Cornish co-founded Freedom's Journal, the first African American newspaper in the U.S. He published The Rights of All as a replacement after Editor John Russwurm emigrated to Liberia.",
+bio: "The Rights of All was an attempt to revive the African American newspaper in New York, however the paper was short-lived, only lasting five issues.",
+storyLink: "http://infoweb.newsbank.com/iw-search/we/HistArchive/HistArchive?d_viewref=doc&p_docnum=-1&p_nbid=S4CA45ECMTQ1NTAwMTEyNy45Mzg1MjoxOjE0OjE1Mi4yMy4xMTAuMTU0&f_docref=v2:12B022C2E1C6B698@EANX-12BF1DA106503218@2389237-12BF1DA10D9A96F8@0&p_docref=v2:12B022C2E1C6B698@EANX-12BF1DA106503218@2389237-12BF1DA10D9A96F8@0",
+storyTitle: "From the Journal of the Times"
+
+},
+{
+name: "Northern Star and Freeman's Advocate",
+startDate: "February 03, 1842",
+endDate: " January 02, 1843",
+numOfIssues: 13,
+location: 'Albany, New York',
+editor: 	'Stephen Myers',
+    images: {
+      editorPic: "http://undergroundrailroadhistory.org/wp-content/uploads2/2010/06/smeyers.jpg",
+      generalPic: "http://undergroundrailroadhistory.org/wp-content/uploads2/2010/06/nstar.jpg"
+          },
+imgCaption: "Stephen Myers was one of the most important leaders of the underground railroad movement in Albany. Born in 1800 in Rensselaer county as a slave, Myers was freed at eighteen and became a leading spokesperson for the anti-slavery movement. ",
+bio: "The Northern Star vehicle for the local free Black community for social and economic betterment and to advocate for the rights of Black citizens. It also attacked the institution of slavery.",
+storyLink: "http://infoweb.newsbank.com/iw-search/we/HistArchive/HistArchive?d_viewref=doc&p_docnum=-1&p_nbid=S4CA45ECMTQ1NTAwMTEyNy45Mzg1MjoxOjE0OjE1Mi4yMy4xMTAuMTU0&f_docref=v2:12B02274CE22BA88@EANX-12BF1D9A27DFDED8@2393870-12BF1D9A2F522A90@0&p_docref=v2:12B02274CE22BA88@EANX-12BF1D9A27DFDED8@2393870-12BF1D9A31E3D5D8@1",
+storyTitle: "A Sterling Truth"
+
+},
+{
+name: "North Star",
+startDate: "December 03, 1847",
+endDate: "February 17, 1860",
+numOfIssues: 237,
+location: 'Rochester, New York',
+editor: 	'Frederick Douglass',
+    images: {
+      editorPic: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Frederick_Douglass_portrait.jpg/800px-Frederick_Douglass_portrait.jpg",
+      generalPic: "https://upload.wikimedia.org/wikipedia/en/thumb/a/ac/NorthStarfrontpage.jpg/170px-NorthStarfrontpage.jpg"
+          },
+imgCaption: "After escaping from slavery in Maryland, Frederick Douglass became a national leader of the abolitionist movement. Douglass was first inspired to publish the North Star after subscribing to The Liberator, a weekly newspaper published by William Lloyd Garrison. ",
+bio: "North Star was an anti-slavery newspaper published by abolitionist Frederick Douglass. Douglass was determined to begin an African-American newspaper that would engage the anti-slavery movement politically.",
+storyLink: "http://infoweb.newsbank.com/iw-search/we/HistArchive/HistArchive?d_viewref=doc&p_docnum=-1&p_nbid=S4CA45ECMTQ1NTAwMTEyNy45Mzg1MjoxOjE0OjE1Mi4yMy4xMTAuMTU0&f_docref=v2:11BE9340B7A005AB@EANX-11D0A35EEBD9A1B8@2395999-11D0A35EFA2F8DB8@0&p_docref=v2:11BE9340B7A005AB@EANX-11D0A35EEBD9A1B8@2395999-11D0A35EFA2F8DB8@0",
+storyTitle: "Reformers by Wandell Phillips"
+},
+{
+name: "New Orleans Daily Creole",
+startDate: "July 01, 1856",
+endDate: "December 31, 1856",
+numOfIssues: 155,
+location: 'New Orleans, Louisiana',
+editor: 	'unknown',
+    images: {
+      editorPic: "unknown",
+      generalPic: "http://medianola.org/uploads/medium/Image2-1380601213.jpg"
+          },
+imgCaption: "unknown",
+bio: "The New Orleans Daily Creole was the first daily newspaper owned and operated by African Americans in New Orleans.",
+storyLink: "http://infoweb.newsbank.com/iw-search/we/HistArchive/HistArchive?d_viewref=doc&p_docnum=-1&p_nbid=S4CA45ECMTQ1NTAwMTEyNy45Mzg1MjoxOjE0OjE1Mi4yMy4xMTAuMTU0&f_docref=v2:11B849020C1891B3@EANX-11B95CB9F4CA57E0@2399163-11B86D13B9F61E98@0&p_docref=v2:11B849020C1891B3@EANX-11B95CB9F4CA57E0@2399163-11B86D13B9F61E98@0-11FAA698F55BF55C@No%20Headline",
+storyTitle: "Legislative Acts/Legal Proceedings"
+},
+{
+name: "Aliened American",
+startDate: "April 09, 1853",
+endDate: "April 09, 1853",
+numOfIssues: 1,
+location: 'Cleveland, Ohio',
+editor: 	'William Howard Day',
+    images: {
+      editorPic: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/William-howard-day.jpg/220px-William-howard-day.jpg",
+      generalPic: "http://research.udmercy.edu/images/php/tn_image.php?width=800&img=http://research.udmercy.edu/digital_collections/baa/14067edi(apr9,1853)(page1).jpg"
+          },
+imgCaption: "William Howard Day was an abolitionist, editor, publisher, printer, teacher, lecturer, civic leader and clergyman who devoted his life to improving the conditions of his fellow African-Americans.",
+bio: "Aliened American was Cleveland's first black newspaper.",
+storyLink: "http://infoweb.newsbank.com/iw-search/we/HistArchive/HistArchive?d_viewref=doc&p_docnum=-1&p_nbid=S4CA45ECMTQ1NTAwMTEyNy45Mzg1MjoxOjE0OjE1Mi4yMy4xMTAuMTU0&f_docref=v2:12B97739D2E2CEF0@EANX-12CC877BA9ECE130@2397953-12CC2D34335FDB00@0&p_docref=v2:12B97739D2E2CEF0@EANX-12CC877BA9ECE130@2397953-12CC2D34335FDB00@0",
+storyTitle: "Ohio State"
+},
+{
+name: "Mirror of the Times",
+startDate: "December 12, 1857",
+endDate: "December 12, 1857",
+numOfIssues: 1,
+location: 'San Francisco, California',
+editor: 	'unknown',
+    images: {
+      editorPic: "unknown",
+      generalPic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAXyIXjCqpDpLr7IXkjqVoQQsBfKhHV-FtQqQIi2JBHIuYW4bX"
+          },
+imgCaption: "unknown",
+bio: "Mirror of the Times was California's first black newspaper.",
+storyLink: "http://infoweb.newsbank.com/iw-search/we/HistArchive/HistArchive?d_viewref=doc&p_docnum=-1&p_nbid=S4CA45ECMTQ1NTAwMTEyNy45Mzg1MjoxOjE0OjE1Mi4yMy4xMTAuMTU0&f_docref=v2:12B7C614F5EA3730@EANX-12CC80B600CA9748@2399661-12CBE6DA63EF1CA8@0&p_docref=v2:12B7C614F5EA3730@EANX-12CC80B600CA9748@2399661-12CBE6DA63EF1CA8@0",
+storyTitle: "The Political Wrongs Inflicted by the American Government Upon its Colored Citizens"
+},
+{
+name: "Black Republican",
+startDate: "April 15, 1865",
+endDate: "May 20, 1865",
+numOfIssues: 5,
+location: 'New Orleans, Louisiana',
+editor: 	'unknown',
+    images: {
+      editorPic: "unknown",
+      generalPic: "http://blog.genealogybank.com/wp-content/uploads/2012/02/black-republican-newspaper-0415-1865-black-slavery.png"
+          },
+imgCaption: "unknown",
+bio: "Black Republican was a newspaper that promoted African-American polotics, literature, art and promoted literacy.",
+storyLink: "http://infoweb.newsbank.com/iw-search/we/HistArchive/HistArchive?d_viewref=doc&p_docnum=-1&p_nbid=S4CA45ECMTQ1NTAwMTEyNy45Mzg1MjoxOjE0OjE1Mi4yMy4xMTAuMTU0&f_docref=v2:12EFC46496A73990@EANX-12F12F5B22C07048@2402342-12EFCD20D6214C68@0&p_docref=v2:12EFC46496A73990@EANX-12F12F5B22C07048@2402342-12EFCD20D6214C68@0",
+storyTitle: "Bloody Chapter of Guerrilla Life in Arkansas"
+},
+
+
   ];
