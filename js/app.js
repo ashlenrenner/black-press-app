35db169a66efbe833f3432eebb4c671ba1b6e33c
ashlen-renner <ashlenrenner@gmail.com>
2016-02-16 10:46:29 -0500

    Created search function and started sorting.

--- js/app.js
+++ js/app.js
@@ -16,18 +16,43 @@ app.controller('BaseController', ['$http', function($http) {
 
     this.panelInfo = true;
     this.name = "";
+    this.sort = "name";
+    this.reverse = false;
+    this.generalPic = "";
+    this.bio = "";
+
+    // Sorting function
+          this.setSort = function(colNam) {
+
+            if (this.sort === colNam) {
+              this.reverse = !this.reverse;
+            }
+
+            this.sort = colNam;
+
+          };
+
 
     //toggle commands for info panel
     this.panelInfo = function(paperInfo){
 
       this.name = paperInfo;
+      this.generalPic = paperInfo;
+      this.bio = paperInfo;
 
       for (var i = 0; i < this.papers.length; i++){
         console.log(i);
         if (paperInfo == this.papers[i].name){
           this.currentPaper = this.papers[i];
           var paperInfo = "";
+
+          if(this.papers[i].generalPic !== undefined){
+            paperInfo += '<img src =' + this.papers[i].generalPic + '/>';
+          }
+
           paperInfo += '<h3>' + this.papers[i].name + '</h3>';
+
+        //  paperInfo += '<p>' this.papers[i].bio + '</p>';
           document.getElementById("paperInfo").innerHTML = paperInfo;
         }
 
